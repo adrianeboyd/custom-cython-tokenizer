45b03921150b482e5e0368ebf0dbe51121b00675
Adriane Boyd <adrianeboyd@gmail.com>
2021-05-03 12:14:24 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,49 @@
+# A minimal custom Cython spaCy tokenizer
+
+This package demonstrates how to create a registered custom tokenizer that
+extends the [spaCy Tokenizer](https://spacy.io/api/tokenizer) in Cython for use
+with spaCy v3.
+
+## Install
+
+```bash
+pip install -U pip setuptools wheel
+pip install .
+```
+
+Or from the repo URL:
+
+```bash
+pip install -U pip setuptools wheel
+pip install https://github.com/adrianeboyd/custom-cython-tokenizer/archive/master.zip
+```
+
+## Usage
+
+Once this package is installed, the custom tokenizer is registered under the
+entry point `spacy_tokenizers`, so you can specify it your config like this:
+
+```ini
+[nlp]
+tokenizer = {"@tokenizers":"custom_tokenizer.v1"}
+```
+
+Or start from a blank model in python:
+
+```python
+import spacy
+
+nlp = spacy.blank("en", config={"nlp": {"tokenizer": {"@tokenizers": "custom_tokenizer.v1"}}})
+```
+
+## Packaging a pipeline
+
+If your packaged spaCy pipeline requires this package, specify it in
+`meta.json` under `requirements` before calling [`spacy
+package`](https://spacy.io/api/cli#package):
+
+```json
+  "requirements":[
+    "custom-tokenizer>=0.0.1,<0.1.0"
+  ]
+```
---
+++ custom_tokenizer/custom_tokenizer.pyx
@@ -0,0 +1,36 @@
+# cython: embedsignature=True, profile=True, binding=True
+from spacy.tokenizer cimport Tokenizer
+
+from typing import Callable
+from spacy.language import Language
+from spacy import util
+
+
+def custom_tokenizer_v1() -> Callable[[Language], "CustomTokenizer"]:
+    """Function to create a tokenizer. Returns a factory that takes the nlp
+    object and returns a Tokenizer instance using the language detaults.
+    """
+
+    def tokenizer_factory(nlp: Language) -> "CustomTokenizer":
+        prefixes = nlp.Defaults.prefixes
+        suffixes = nlp.Defaults.suffixes
+        infixes = nlp.Defaults.infixes
+        prefix_search = util.compile_prefix_regex(prefixes).search if prefixes else None
+        suffix_search = util.compile_suffix_regex(suffixes).search if suffixes else None
+        infix_finditer = util.compile_infix_regex(infixes).finditer if infixes else None
+        return CustomTokenizer(
+            nlp.vocab,
+            rules=nlp.Defaults.tokenizer_exceptions,
+            prefix_search=prefix_search,
+            suffix_search=suffix_search,
+            infix_finditer=infix_finditer,
+            token_match=nlp.Defaults.token_match,
+            url_match=nlp.Defaults.url_match,
+        )
+
+    return tokenizer_factory
+
+
+cdef class CustomTokenizer(Tokenizer):
+    def __call__(self, unicode string):
+        return self._tokenize_affixes(string, True)
